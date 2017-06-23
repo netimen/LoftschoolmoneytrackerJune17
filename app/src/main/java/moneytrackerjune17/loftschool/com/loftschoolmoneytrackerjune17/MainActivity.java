f7d1b807a322319707fe72954256f30f4149c639
Dmitry Gordeev <netimen@gmail.com>
2017-06-23 21:35:32 +0300

    display content

--- app/src/main/java/moneytrackerjune17/loftschool/com/loftschoolmoneytrackerjune17/MainActivity.java
+++ app/src/main/java/moneytrackerjune17/loftschool/com/loftschoolmoneytrackerjune17/MainActivity.java
@@ -2,41 +2,70 @@ package moneytrackerjune17.loftschool.com.loftschoolmoneytrackerjune17;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.widget.EditText;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final TextView add = (TextView) findViewById(R.id.add);
-        final EditText name = (EditText) findViewById(R.id.name);
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                add.setEnabled(!TextUtils.isEmpty(s));
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        setContentView(R.layout.items);
+        final RecyclerView items = (RecyclerView) findViewById(R.id.items);
+        items.setAdapter(new ItemsAdapter());
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private class ItemsAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+        final List<Item> items = new ArrayList<>();
+
+        ItemsAdapter() {
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+            items.add(new Item("car", 100));
+            items.add(new Item("apple", 400));
+        }
+
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ItemViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, null));
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            final Item item = items.get(position);
+            holder.name.setText(item.name);
+            holder.price.setText(String.valueOf(item.price));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
     }
 
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView name, price;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+            price = (TextView) itemView.findViewById(R.id.price);
+        }
+    }
 }
